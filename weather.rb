de378883dc377ce67dfa6507e6392afe666ae9ef
april-guerin <aprilguerin@yahoo.com>
2018-07-26 18:49:38 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -38,7 +38,7 @@ end
 # Hint - how can we find the algorithm to convert Farenheit to Celsius?
 
 def convert_temp(weather)
-  puts weather[:farenheit]
+  puts weather[:farenheit].to_f
   farenheit = weather[:farenheit].to_f
   puts farenheit
   (farenheit-32)*(5/9)
